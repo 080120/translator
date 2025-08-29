95eed86fe5fc4d659e603117c424c43a657b675e
080120 <riochankawaii2001@gmail.com>
2025-08-29 11:38:24 +0700

    update server.py

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from deep_translator import GoogleTranslator
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello, FastAPI chạy thành công 🚀"}
+    return {"message": "Hello, FastAPI on Render 🚀"}
+
+@app.get("/translate")
+def translate(text: str, target: str = "vi"):
+    translated = GoogleTranslator(source="auto", target=target).translate(text)
+    return {"original": text, "translated": translated, "target_lang": target}
